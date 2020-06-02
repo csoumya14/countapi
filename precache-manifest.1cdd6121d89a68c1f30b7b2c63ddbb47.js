3d32e12839b2e3b4b125449c468bb188009652b8
Soumya Chalakkal <soumyachalakkal@binoys-Air.lan>
2020-06-02 12:35:14 +0200

    Updates

---
+++ precache-manifest.1cdd6121d89a68c1f30b7b2c63ddbb47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40723323617751b1bb61883c1dcb24f",
+    "url": "/countapi/index.html"
+  },
+  {
+    "revision": "52390ab3d7cc7607f4d7",
+    "url": "/countapi/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "bb745d5a220121edf259",
+    "url": "/countapi/static/css/main.0bcfd068.chunk.css"
+  },
+  {
+    "revision": "52390ab3d7cc7607f4d7",
+    "url": "/countapi/static/js/2.c35c1600.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/countapi/static/js/2.c35c1600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb745d5a220121edf259",
+    "url": "/countapi/static/js/main.dee68b3a.chunk.js"
+  },
+  {
+    "revision": "18d49282d349c6ee456b",
+    "url": "/countapi/static/js/runtime-main.a2ffc819.js"
+  }
+]);
